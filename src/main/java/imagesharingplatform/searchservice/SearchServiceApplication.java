81c8ef3dfa3ae67eac55d404bb301c12e90ae74a
Tien Huynh <52915164+anhtienzz123@users.noreply.github.com>
2023-06-03 17:46:57 +0700

    first commit

---
+++ src/main/java/imagesharingplatform/searchservice/SearchServiceApplication.java
@@ -0,0 +1,13 @@
+package imagesharingplatform.searchservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SearchServiceApplication.class, args);
+	}
+
+}
